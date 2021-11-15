8f88879fa63426756572c87742aa08aaf4085b61
Try Ajitiono <ballinst@gmail.com>
2021-11-15 09:19:20 +0700

    ok
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,8 @@ app.use(cors());
 app
     .get('/', async (req, res) => {
         res.json({
-            version: '1.0.0',
-            url: 'https://gofood.co.id/gofood/web/v1/restaurants',
-            path: ['random', 'merchant/:id']
+            hello: 'world'
         });
-    })
-    .get('/random', async (req, res) => {
-        const { lat, long } = req.query;
-        if (!lat || !long) return res.status(406).json({ error: 'Provide lat and long in query' });
-
-        const food = new RandomGoFood(lat, long);
-        const merchants = await food.fastestMerchants();
-
-        res.json(merchants);
-    })
-    .get('/merchant/:id', async (req, res) => {
-        const { id } = req.params;
-        const detail = await RandomGoFood.detailMerchants(id);
-
-        res.json(detail);
     })
 
 const PORT = process.env.PORT || 3001;
